1175329dd4269ba0744e4765800f4351b0a1967b
Edison Parra <edisonparraortiz8@gmail.com>
2022-08-03 03:09:45 -0500

    cambios en el contenido

--- script.js
+++ script.js
@@ -23,10 +23,10 @@ function responsiveMenu(){
 }
 
 window.onscroll =function(){
-    efectohabilidades()
+    efectoHabilidades()
 };
 
-function efectohabilidades(){
+function efectoHabilidades(){
     var skills =document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
@@ -35,8 +35,12 @@ function efectohabilidades(){
         document.getElementById("bd").classList.add("barra-progreso3")
         document.getElementById("ps").classList.add("barra-progreso4")
         document.getElementById("rc").classList.add("barra-progreso5")
+        document.getElementById("gt").classList.add("barra-progreso5")
+        document.getElementById("gt").classList.add("barra-progreso6")
+        document.getElementById("css").classList.add("barra-progreso7")
     }
 }
+
 
 
 const hamburger_icon = document.querySelector('#nav-bar .nav-bar .nav-list .hamburger');
